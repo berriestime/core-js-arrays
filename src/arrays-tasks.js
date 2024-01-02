5a8b5954547cd1baae55a86b2408f6e16b39d7a7
Clukva <berries.whale@gmail.com>
2024-01-02 18:15:02 +0300

    feat: add sumArrays function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -39,8 +39,14 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const maxLength = Math.max(arr1.length, arr2.length);
+
+  return Array.from({ length: maxLength }, (_, index) => {
+    const value1 = arr1[index] !== undefined ? arr1[index] : 0;
+    const value2 = arr2[index] !== undefined ? arr2[index] : 0;
+    return value1 + value2;
+  });
 }
 
 /**
